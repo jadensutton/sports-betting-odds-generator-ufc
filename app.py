fc699828152973dd0f062c0749765e4f13c82978
Jaden Sutton <jadensutton_3@hotmail.com>
2022-03-28 23:09:02 -0400

    Fixed bug

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import requests
 
 from bs4 import BeautifulSoup
 from flask import request
+from flask.helpers import send_from_directory
 from flask_cors import CORS, cross_origin
 
 app = flask.Flask(__name__, static_folder='../client/build', static_url_path='')
